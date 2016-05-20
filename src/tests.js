676cd5775c2ff459381922d7c789afbff01b0f3e
Clayton Gibb <cgibb@homelab.com>
2016-05-20 11:36:06 -0700

    adding errors if get and set are not defined in init call

--- src/tests.js
+++ src/tests.js
@@ -1,3 +1,4 @@
+"use strict";
 var Tests = {
     // numTests is number of different test states, numTests = 2 is a/b, numTests = 3 is a/b/c
     // they are distributed evenly so numTests = 2 is 50/50, numTests = 3 is 33/33/33, etc
@@ -5,11 +6,10 @@ var Tests = {
     // testID should be defined as it will be default in case of an error
     testID: 0,
     dbGetfunction: function() {
-        console.log("dbGetfunction not set");
-        return 1;
+        throw new Error("dbGetfunction not set");
     },
     dbSetfunction: function(value) {
-        console.log("dbSetfunction not set");
+         throw new Error("dbSetfunction not set");
     },
     init: function(getter, setter, numTests) {
         this.dbGetfunction = getter;
